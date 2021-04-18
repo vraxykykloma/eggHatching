5b28a1662209df6403212d106fe3e6c4ee10285b
vraxykykloma <82768590+vraxykykloma@users.noreply.github.com>
2021-04-18 22:34:05 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+var egg{
+  var chaotic{
+    var forest = 0 , abyss = 0 , fortress = 0 , shadow = 0 ;
+  }
+  var entangled{
+    var warrior = 0 , mage = 0 , ranger = 0 , assassin = 0 , priest = 0 ;
+  }
+  var sober{
+    var trans = 0 , light = 0 , dark = 0 ;
+  }
+}
+
+var mat1 = 0, mat2 = 0 , mat3 = 0 , mat4 = 0 ;
+
+function mat(x){
+  switch x{
+      
+    
