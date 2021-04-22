07d2600554624da7c4a31633cbcc3afda12606ac
vraxykykloma <82768590+vraxykykloma@users.noreply.github.com>
2021-04-22 23:53:28 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,37 @@
-var egg{
-  var chaotic{
-    var forest = 0 , abyss = 0 , fortress = 0 , shadow = 0 ;
+var collection = {
+  chaotic : ["forest" , "abyss" , "fortress" , "shadow"],
+  entangled : ["warrior" , "mage" , "ranger" , "assassin" , "priest"],
+  sober : ["trans" , "light" , "dark"],
+  acquired : [],
+  mats : [0,0,0,0],
+  dust : 0 // in thousands
+}
+var egg = 0; //move to html probably, 1 = chaotic, 2 = entangled, 3 = sober
+var pityT = 0; // pity timer
+
+function hatch(egg){
+  pityT++;
+  var pull = Math.random();
+  if (pull < 0.06 || pityT == 15){
+    // egg/mats pull
+    switch egg{
+      case 1://chaotic
+        if (collection.chaotic.length > 0){
+          var ssRng = Math.floor(Math.random() * collection.chaotic.length);  // determines what kind of starspawn we got
+          collection.acquired.push( collection.chaotic.splice(ssRng-1 , 1) ); // removes the ss from the missing array and puts it in the aquired one
+        }
+        break;
+        default;
+    }
   }
-  var entangled{
-    var warrior = 0 , mage = 0 , ranger = 0 , assassin = 0 , priest = 0 ;
+  else if(pull < 0.2){
+    // mats pull
+    var matRng = Math.floor(Math.random() * 4);  // determines what kind of mat we got
+    collection.mats[matRng] += 10 * Math.pow(2 , matRng); // adds 10 for type1, 20 for type2, 40 for type3, 80 for type4
   }
-  var sober{
-    var trans = 0 , light = 0 , dark = 0 ;
+  else {
+    // dust pull
+    collection.dust += 7;
+    
   }
 }
-
-var mat1 = 0, mat2 = 0 , mat3 = 0 , mat4 = 0 ;
-
-function mat(x){
-  switch x{
-      
-    
